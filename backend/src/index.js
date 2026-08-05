10675712660e96dbc03bdad562c63e82b6046380
tainguyen04 <huutai18102004@gmail.com>
2026-08-05 16:25:10 +0700

    Refactor to use ES6 import syntax for Express and add JSON middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
