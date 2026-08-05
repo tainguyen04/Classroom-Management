e041f3584bcff296477f471ef6c39343304d9933
tainguyen04 <huutai18102004@gmail.com>
2026-08-05 16:44:43 +0700

    Refactor Firebase integration: remove old config, add service account initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
