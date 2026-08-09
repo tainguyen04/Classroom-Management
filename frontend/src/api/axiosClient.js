348ba3da6c3a7a83faf69bed7e6b33c895807afd
Tai <huutai18102004@gmail.com>
2026-08-09 20:26:23 +0700

    Fix baseURL in axiosClient configuration
    Removed trailing slash from baseURL in axiosClient.

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 export const axiosClient = axios.create({
-  baseURL: "https://classroom-management-bg6r.onrender.com/api/",
+  baseURL: "https://classroom-management-bg6r.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
