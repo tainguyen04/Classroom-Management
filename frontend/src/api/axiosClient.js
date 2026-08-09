adebb80289486289378fddd7912fbe55773788ac
Tai <huutai18102004@gmail.com>
2026-08-09 20:25:49 +0700

    Update axios baseURL to include '/api/'

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 export const axiosClient = axios.create({
-  baseURL: "https://classroom-management-bg6r.onrender.com",
+  baseURL: "https://classroom-management-bg6r.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
