2b930eb89ac315c6f3e93ef035e660ab9a7fb4b5
tainguyen04 <huutai18102004@gmail.com>
2026-08-10 08:13:57 +0700

    fix: update CORS configuration to support multiple origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import socketRoutes from "./socket/socket.route.js";
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   }),
 );
 const server = http.createServer(app);
