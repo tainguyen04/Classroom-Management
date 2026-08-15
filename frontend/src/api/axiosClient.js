9fdcb8f0e1a284de6ab937292d9ed70add8010b9
tainguyen04 <huutai18102004@gmail.com>
2026-08-15 09:19:17 +0700

    feat: update axiosClient to use environment variable for baseURL

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import queryString from "query-string";
 export const axiosClient = axios.create({
-  baseURL: "https://classroom-management-bg6r.onrender.com/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://classroom-management-bg6r.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
